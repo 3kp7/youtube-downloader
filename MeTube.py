24f64952bfbfa00d6dac2e4542117b2edc2216d2
Abdelrahman <86517323+3kp7@users.noreply.github.com>
2025-02-26 21:09:24 +0000

    first function

--- MeTube.py
+++ MeTube.py
@@ -2,7 +2,7 @@ import os
 
 import telebot
 
-import request
+from pytube import Youtube
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
@@ -12,13 +12,20 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def send_welcome(message):
     bot.reply_to(message, "Mp3 or Mp4 File")
 
-@bot.messsage_handler(commands=['Mp3'])
+@bot.message_handler(commands=['Mp3'])
 def send_message(message):
     bot.reply_to(message, "Provide me with link")
 
 @bot.message_handler(commands=['Mp4'])
 def send_message(message):
     bot.reply_to(message, "Provide me with link")
+
+def mp3_video(url):
+    video_caller = Youtube(url)
+    print(video_caller.title)
+    video_caller.stream.filter(progressive=True,extention="mp3".order_by(
+            'resolution').desc().first().download())
+            print("done")
 
     
 bot.infinity_polling()
