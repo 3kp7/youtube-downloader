703acc30dc8057fa1aec635cb66d08420e9c3475
Abdelrahman <86517323+3kp7@users.noreply.github.com>
2025-02-26 20:58:03 +0000

    added message handler

--- MeTube.py
+++ MeTube.py
@@ -2,12 +2,23 @@ import os
 
 import telebot
 
+import request
+
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Mp3 or Mp4 File")
+
+@bot.messsage_handler(commands=['Mp3'])
+def send_message(message):
+    bot.reply_to(message, "Provide me with link")
+
+@bot.message_handler(commands=['Mp4'])
+def send_message(message):
+    bot.reply_to(message, "Provide me with link")
+
     
 bot.infinity_polling()
